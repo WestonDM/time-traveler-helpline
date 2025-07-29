1169b080b02978d78eecd7cfba6a68c95d3a4fdf
Chris Weston <cweston@westondigitalmedia.com>
2025-07-29 13:07:27 -0400

    Pull rando word from App.js

--- src/Randos.js
+++ src/Randos.js
@@ -4,18 +4,12 @@ export default function Randos({ randoWords }) {
             <div id="randos">
                 <h2 className="h1 firstQ_header">This Week's Random Words</h2>
                 <>
-                    <div className="rando_word">
-                        <img src="./img/1.svg" alt="1" />
-                        <h2>Door</h2>
-                    </div>
-                    <div className="rando_word">
-                        <img src="./img/2.svg" alt="2" />
-                        <h2>Earth</h2>
-                    </div>
-                    <div className="rando_word">
-                        <img src="./img/3.svg" alt="3" />
-                        <h2>Horse</h2>
-                    </div>
+                    {randoWords.map((word) => (
+                        <div className="rando_word" key={word.id}>
+                            <img src={`./img/${word.id}.svg`} alt={word.id} />
+                            <h2>{word.word}</h2>
+                        </div>
+                    ))}
                 </>
             </div>
         </div>
