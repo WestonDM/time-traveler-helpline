e9ea2925ae8754d176f01a08219ffcc6bee6730b
Chris Weston <cweston@westondigitalmedia.com>
2025-07-29 18:13:19 -0400

    Current time

--- src/CurrentTime.js
+++ src/CurrentTime.js
@@ -1,6 +1,24 @@
+import { useState, useEffect } from "react";
+
 export default function CurrentTime() {
-    const today = new Date();
-    const formattedTime = today.toLocaleTimeString(); // e.g., "3:01:00 PM"
+    const [currentTime, setCurrentTime] = useState(new Date());
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCurrentTime(new Date());
+        }, 1000);
+
+        // Cleanup function to clear the interval when component unmounts
+        return () => clearInterval(timer);
+    }, []);
+
+    const formattedTime = currentTime.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false,
+    }); // e.g., "3:01:00 PM"
+
     return (
         <div className="date_display">
             <h4>{formattedTime}</h4>
