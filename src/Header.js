a1659f7433c54bce0338aa4c494b7309110c9ea8
Chris Weston <cweston@westondigitalmedia.com>
2025-07-30 15:42:07 -0400

    Header updates

--- src/Header.js
+++ src/Header.js
@@ -7,24 +7,38 @@ export default function Header() {
             <div className="header">
                 <CurrentDate />
                 <CurrentTime />
-                <h1>
-                    <span className="h3">Welcome to</span>
-                    <br />
-                    <span className="h1">THE TIME TRAVELER’S HELPLINE</span>
-                </h1>
-                <h4>
-                    The premiere online resource for the waylaid temporal
-                    wayfarer.
-                </h4>
-                <p>
-                    While time travel has not been invented yet, the need for
-                    guidance for future travelers is clear. That’s where we come
-                    in. By identifying yourself as a genuine time traveler, you
-                    will be granted access to exclusive resources to help you
-                    navigate this era. A personal temporal consultant will be
-                    assigned to you to help ease your way through this
-                    particular moment in time.
-                </p>
+                <div id="main-hl">
+                    <h1>
+                        <span className="h3">Welcome to</span>
+                        <br />
+                        <span className="h1">THE TIME TRAVELER’S HELPLINE</span>
+                    </h1>
+                    <h3>
+                        The premiere online resource for the waylaid temporal
+                        wayfarer.
+                    </h3>
+                </div>
+                <div id="about">
+                    <p>
+                        <span className="h5">
+                            Time travel might not be a thing (yet), but for the
+                            seasoned temporal sojourner, <em>“yet”</em> is for
+                            suckers.
+                        </span>
+                    </p>
+                    <p>
+                        That said, it's not for the faint of heart. Navigating
+                        the complexities of time travel requires a certain level
+                        of expertise and experience. That’s where we come in. By
+                        identifying yourself as a genuine, verified,
+                        authenticated time traveler, you will be granted access
+                        to exclusive resources to help you navigate this era.
+                        You'll be paired with your very own temporal
+                        consultant—a savvy guide to help you navigate the
+                        quirks, customs, and potential paradoxes of the current
+                        timeline.
+                    </p>
+                </div>
             </div>
         </header>
     );
